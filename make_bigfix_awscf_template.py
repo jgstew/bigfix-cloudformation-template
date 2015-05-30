0950a8c7833ff22e9e4a6f52d30d4e761536a74e
JGStew <james@jgstew.com>
2015-05-30 15:18:45 +0000

    refactored to use a function

--- make_bigfix_awscf_template.py
+++ make_bigfix_awscf_template.py
@@ -10,41 +10,55 @@ from troposphere import GetAtt, Parameter, Output, Ref, Tags, Template
 
 import troposphere.ec2 as ec2
 
-template = Template()
+def make_bigfix_awscf_template():
+    template = Template()
 
-template.add_description("""\
+    template.add_description("""\
 BigFix Eval AWS CloudFormation Template within the resource limits of the AWS free tier.  \
 **WARNING** This template creates Amazon EC2 & RDS instances. You may be billed \
 for the AWS resources used if you create a stack from this template.""")
 
-template.add_version('2010-09-09')
+    template.add_version('2010-09-09')
 
-ec2_instance = ec2.Instance("BigFixEval")
-ec2_instance.ImageId = "ami-6502e021"
-ec2_instance.InstanceType = "t2.micro"
+    ec2_instance = ec2.Instance("BigFixEval")
+    ec2_instance.ImageId = "ami-6502e021"
+    ec2_instance.InstanceType = "t2.micro"
 
-template.add_resource(ec2_instance)
+    template.add_resource(ec2_instance)
 
 
-template.add_output([
-    Output(
-        "AZ",
-        Description="Availability Zone of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "AvailabilityZone"),
-    ),
-    Output(
-        "PublicDNS",
-        Description="Public DNSName of the newly created EC2 instance",
-        Value=GetAtt(ec2_instance, "PublicDnsName"),
-    ),
-])
+    template.add_output([
+        Output(
+            "AZ",
+            Description="Availability Zone of the newly created EC2 instance",
+            Value=GetAtt(ec2_instance, "AvailabilityZone"),
+        ),
+        Output(
+            "PublicDNS",
+            Description="Public DNSName of the newly created EC2 instance",
+            Value=GetAtt(ec2_instance, "PublicDnsName"),
+        ),
+    ])
 
-template.add_parameter(
-    Parameter(
-        'RootServerDomain',
-        Description=' The domain name the BigFix root server will use ',
-        Type='String',
-        Default='bigfixeval.organization.tld'
-    ))
+    template.add_parameter(
+        Parameter(
+            'RootServerDomain',
+            Description=' The domain name the BigFix root server will use ',
+            Type='String',
+            Default='bigfixeval.organization.tld'
+        ))
 
-print( template.to_json() )
+    return template.to_json()
+
+
+
+
+if __name__ == '__main__':
+    print ""
+    strResult = make_bigfix_awscf_template()
+    print( strResult )
+    print ""
+    
+    f=open("bf_cf.template.json",'w')
+    f.write(strResult+'\n')
+    f.close();
