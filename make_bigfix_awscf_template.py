92bf66704f71949a509682bd2cadb8da7717dfe8
jgstew <james@jgstew.com>
2015-05-30 10:59:20 -0400

    Create make_bigfix_awscf_template.py

---
+++ make_bigfix_awscf_template.py
@@ -0,0 +1,50 @@
+
+# https://github.com/cloudtools/troposphere
+# https://gist.github.com/jgstew/aff5da6a34c82e60fc75
+# https://github.com/cloudtools/troposphere/blob/master/examples/VPC_single_instance_in_subnet.py
+
+# region=us-west-1
+# shutdown behavoir = stop
+# Protect against accidental termination
+from troposphere import GetAtt, Parameter, Output, Ref, Tags, Template
+
+import troposphere.ec2 as ec2
+
+template = Template()
+
+template.add_description("""\
+BigFix Eval AWS CloudFormation Template within the resource limits of the AWS free tier.  \
+**WARNING** This template creates Amazon EC2 & RDS instances. You may be billed \
+for the AWS resources used if you create a stack from this template.""")
+
+template.add_version('2010-09-09')
+
+ec2_instance = ec2.Instance("BigFixEval")
+ec2_instance.ImageId = "ami-6502e021"
+ec2_instance.InstanceType = "t2.micro"
+
+template.add_resource(ec2_instance)
+
+
+template.add_output([
+    Output(
+        "AZ",
+        Description="Availability Zone of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "AvailabilityZone"),
+    ),
+    Output(
+        "PublicDNS",
+        Description="Public DNSName of the newly created EC2 instance",
+        Value=GetAtt(ec2_instance, "PublicDnsName"),
+    ),
+])
+
+template.add_parameter(
+    Parameter(
+        'RootServerDomain',
+        Description=' The domain name the BigFix root server will use ',
+        Type='String',
+        Default='bigfixeval.organization.tld'
+    ))
+
+print( template.to_json() )
